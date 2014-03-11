2cd0e1e747c95132bbda4ff0e63973d314372f7c
kjgilbert <kgilbert@zoology.ubc.ca>
2014-03-11 12:56:03 -0700

    finished chapter 1 of tutorials

--- ThirdProgram.cpp
+++ ThirdProgram.cpp
@@ -9,6 +9,22 @@
 	#define identifier replacement
 		this makes any further occurrence of 'identifier' to be replaced by 'replacement'
 		usually the identifier is typed in all caps with underscores for spaces
+		
+		this is helpful because it can be used to give a descriptive name to something like a number (e.g. a hard-coded number / magic number that does not have any context within the code, but comes for some outside reason)
+	
+	e.g. the #undef ....
+	#undef will undefine a previously defined value
+	
+	conditional compilation:
+	#ifdef		allows the preprocessor to cehck if a value has been previously defined, if so it compiles anything between this and the #endif
+	#ifndef		the opposite of above, compile section if item is not defined
+	#endif
+	
+	header guards - prevent the same header from being brought in twice which can cause the compiler to fail
+		#ifndef UNIQUE_NAME_HERE  --- if the header has not been called previously, it is included, otherwise it isn't
+		#define UNIQUE_NAME_HERE
+		// declaratiosn here
+		#endif
 
 */
 
@@ -16,12 +32,14 @@
 #include <iostream>
 
 #define MY_NAME "Kim"
+#define YEN_PER_DOLLAR 122
 	
 int main()
 {
 	using namespace std;
 	
 	cout << "Hello, " << MY_NAME << endl;
+	int nYen = nDollars * YEN_PER_DOLLAR;
 	
 	return 0;
 }
